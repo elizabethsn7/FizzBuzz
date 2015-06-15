1c41d189d1b09b6fb1268cf919f5bc8dc1d8bff0
Elizabeth Kipp <liz@elizabethkipp.com>
2015-06-15 13:59:21 -0500

    23

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function (){
+
+	function isFizzBuzz(number) {
+		return number % 3 === 0 && number % 5 === 0;
+	}
+
+	function isFizz(number) {
+		return number % 3 === 0;
+	}
+
+	function isBuzz(number) {
+		return number % 5 === 0;
+	}
+
+	for (i=1; i <= 100; i++){
+		if (isFizzBuzz(i)) {
+			console.log("fizzbuzz");
+		} else if (isFizz(i)) {
+			console.log("Fizz");
+		} else if (isBuzz(i)) {
+			console.log("Buzz");
+		} else { 
+			console.log(i); 
+		}
+
+	}
+
+});
