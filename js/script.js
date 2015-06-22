f62e0dc51aee69daaa77dde6b048ae91b44ab217
Elizabeth Kipp <liz@elizabethkipp.com>
2015-06-22 14:17:57 -0500

    refractor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+	var list = $('ul');
 
 	function isFizzBuzz(number) {
 		return number % 3 === 0 && number % 5 === 0;
@@ -12,31 +13,33 @@ $( document ).ready(function() {
 		return number % 5 === 0;
 	}
 
-$('form').submit(function(e) {
-	e.preventDefault();
-	
-	if ($('.iteminput').val() === '') {
-			console.log('empty item');
-		}
-
-
-	var maxnumber = $('.iteminput').val();
-
-	$('.input-number').click(function() {
+	$('form').submit(function(e) {
+		e.preventDefault(); 
+		var maxnumber = $('.iteminput').val();
+			// maxnumber = +(maxnumber);
+			// console.log(maxnumber); this is parseInt 
 		
-	})
-	
-	for (i=1; i <= 100; i++){
-		if (isFizzBuzz(i)) {
-			console.log("fizzbuzz");
-		} else if (isFizz(i)) {
-			console.log("Fizz");
-		} else if (isBuzz(i)) {
-			console.log("Buzz");
+		if ($.isNumeric(maxnumber) === false  || maxnumber >100 ) {
+			console.log('empty item');
+
+
 		} else { 
-			console.log(i); 
+			for (i=1; i <= 100; i++){
+				if (isFizzBuzz(i)) {
+					list.append('<li>FizzBuzz</li>')
+					console.log("fizzbuzz");
+				} else if (isFizz(i)) {
+					list.append('<li>Fizz</li>')
+					console.log("Fizz");
+				} else if (isBuzz(i)) {
+					list.append('<li>Buzz</li>')
+					console.log("Buzz");
+				} else { 
+					list.append('<li>' + i + '</li>')
+					console.log(i); 
+				}
+			}
 		}
-	}	
 
 	});
 
